92cfa90118d6d00cc544296a3ba6ca2ad284bc4d
satoshi <sydpqb3104nnn@gmail.com>
2019-01-17 04:41:53 +0900

    Constantize number of records

--- main.go
+++ main.go
@@ -7,31 +7,36 @@ import (
 	"log"
 	"time"
 
+	_ "github.com/lib/pq"
+
 	"github.com/istsh/go-sqlboiler-example/models"
 
 	"github.com/volatiletech/null"
 	"github.com/volatiletech/sqlboiler/boil"
 )
 
+const RecordCount = 100
+
 func main() {
 	db, err := sql.Open("postgres", "host=localhost port=5432 user=dev dbname=postgres password=pass sslmode=disable")
 	if err != nil {
 		log.Fatalln(err)
 	}
+	boil.SetDB(db)
 	defer db.Close()
 
 	now := time.Now()
-	// dropTable(db)
-	// timeAfterDropTable := time.Now()
-	// fmt.Printf("dropTable: %s\n", timeAfterDropTable.Sub(now).String())
+	dropTable(db)
+	timeAfterDropTable := time.Now()
+	fmt.Printf("dropTable: %s\n", timeAfterDropTable.Sub(now).String())
 
-	// migrate(db)
-	// timeAfterMigrate := time.Now()
-	// fmt.Printf("migrate: %s\n", timeAfterMigrate.Sub(timeAfterDropTable).String())
+	migrate(db)
+	timeAfterMigrate := time.Now()
+	fmt.Printf("migrate: %s\n", timeAfterMigrate.Sub(timeAfterDropTable).String())
 
 	insert(db)
 	timeAfterInsert := time.Now()
-	fmt.Printf("insert: %s\n", timeAfterInsert.Sub(now).String())
+	fmt.Printf("insert: %s\n", timeAfterInsert.Sub(timeAfterMigrate).String())
 
 	selectAndUpdate(db)
 	timeAfterSelectAndUpdate := time.Now()
@@ -42,19 +47,39 @@ func main() {
 	fmt.Printf("selectAndDelete: %s\n", timeAfterSelectAndDelete.Sub(timeAfterSelectAndUpdate).String())
 }
 
-// func dropTable(db *sql.DB) {
-// 	// テーブルを削除
-// 	db.DropTable(&User{})
-// }
+func dropTable(db *sql.DB) {
+	// テーブルを削除
+	_, err := db.Query("DROP TABLE IF EXISTS users")
+	if err != nil {
+		fmt.Printf("drop table err: %s\n", err.Error())
+		return
+	}
+}
 
-// func migrate(db *gorm.DB) {
-// 	// テーブルを作成
-// 	db.AutoMigrate(&User{})
-// }
+func migrate(db *sql.DB) {
+	// テーブルを作成
+	sql := `
+	CREATE TABLE users (
+		id serial,
+		created_at timestamp with time zone,
+		updated_at timestamp with time zone,
+		deleted_at timestamp with time zone,
+		name text,
+		PRIMARY KEY (id)
+	);
+	
+	CREATE INDEX idx_users_deleted_at ON users(deleted_at);
+	`
+	_, err := db.Query(sql)
+	if err != nil {
+		fmt.Printf("migrate err: %s\n", err.Error())
+		return
+	}
+}
 
 func insert(db *sql.DB) {
 	ctx := context.Background()
-	for i := 1; i <= 100; i++ {
+	for i := 1; i <= RecordCount; i++ {
 		// now := null.NewTime(time.Now(), true)
 		u := models.User{
 			// CreatedAt: now,
@@ -71,17 +96,17 @@ func insert(db *sql.DB) {
 
 func selectAndUpdate(db *sql.DB) {
 	ctx := context.Background()
-	for i := 1; i <= 100; i++ {
+	for i := 1; i <= RecordCount; i++ {
 		u, err := models.FindUser(ctx, db, i)
 		if err != nil {
-			fmt.Printf("err(id = %d): %s\n", i, err.Error())
+			fmt.Printf("select err(id = %d): %s\n", i, err.Error())
 			continue
 		}
 		u.Name = null.NewString(fmt.Sprintf("sqlboiler_test_user_%03d_updated", i), true)
 		// データを更新
 		_, err = u.Update(ctx, db, boil.Infer())
 		if err != nil {
-			fmt.Printf("err(id = %d): %s\n", i, err.Error())
+			fmt.Printf("update err(id = %d): %s\n", i, err.Error())
 			continue
 		}
 	}
@@ -89,16 +114,16 @@ func selectAndUpdate(db *sql.DB) {
 
 func selectAndDelete(db *sql.DB) {
 	ctx := context.Background()
-	for i := 1; i <= 100; i++ {
+	for i := 1; i <= RecordCount; i++ {
 		u, err := models.FindUser(ctx, db, i)
 		if err != nil {
-			fmt.Printf("err(id = %d): %s\n", i, err.Error())
+			fmt.Printf("select err(id = %d): %s\n", i, err.Error())
 			continue
 		}
 		// データを削除
 		_, err = u.Delete(ctx, db)
 		if err != nil {
-			fmt.Printf("err(id = %d): %s\n", i, err.Error())
+			fmt.Printf("delete err(id = %d): %s\n", i, err.Error())
 			continue
 		}
 	}
